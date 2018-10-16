593688767d991551a743f7dd469237b207f079f4
Charlie Wray <charlesmwray@gmail.com>
2018-10-16 12:56:51 -0700

    Updates

---
+++ precache-manifest.fd0d517691915c73a35cff67871bcba6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b238917f14b9f1039cd5",
+    "url": "/mktracker/static/js/runtime~main.b238917f.js"
+  },
+  {
+    "revision": "cc3152860ca845dc89fc",
+    "url": "/mktracker/static/js/main.cc315286.chunk.js"
+  },
+  {
+    "revision": "f4708600001996acf2a4",
+    "url": "/mktracker/static/js/1.f4708600.chunk.js"
+  },
+  {
+    "revision": "cc3152860ca845dc89fc",
+    "url": "/mktracker/static/css/main.d491e44f.chunk.css"
+  },
+  {
+    "revision": "7162b47144b9aa39673d5eb19cb8d837",
+    "url": "/mktracker/index.html"
+  }
+];
