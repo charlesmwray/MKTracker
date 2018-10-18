3a04fcd657c295c85ea3382e4ba66b95557eb41c
Alex <info@ezwebsupport.com>
2018-10-18 22:19:56 +0000

    layout changes and fix of number after the dot

--- src/App.js
+++ src/App.js
@@ -41,37 +41,37 @@ class App extends Component {
     render() {
         return (
             <div class="container">
-            <div className="App">
-                <header className="App-header">
-                    <Row>
-                        <Col xs={12} sm={8}>
-                            <h1>Mario Kart Tracker</h1>
-                        </Col>
-                        <Col xs={12} sm={4}>
-                            {!this.state.userInfo && <Button bsStyle="primary" onClick={() => {this.login()}}>Log In</Button>}
-                            {this.state.userInfo && <div className="pull-right"><img style={{ margin: '1rem', width: '50px', height: '50px', borderRadius: '50%' }} src={this.state.userInfo.photoURL} /></div>}
-                        </Col>
-                    </Row>
-                </header>
-                {
-                    this.state.userInfo &&
-                    <div>
-                        {
-                            !this.state.showAddScore &&
-                            <Button bsStyle="primary" onClick={() => {this.toggleAddScore()}}>Add Score</Button>
-                        }
-
-                        {
-                            this.state.showAddScore &&
-                            <EnterScore
-                                userInfo={this.state.userInfo}
-                                toggleAddScore={this.toggleAddScore.bind(this)}
-                            />
-                        }
-                    </div>
-                }
-                <LeaderBoard />
-            </div>
+                <div className="App">
+                    <header className="App-header">
+                        <Row>
+                            <Col xs={12} sm={8}>
+                                <h1>Mario Kart Tracker</h1>
+                            </Col>
+                            <Col xs={12} sm={4}>
+                                {!this.state.userInfo && <Button bsStyle="primary" onClick={() => {this.login()}}>Log In</Button>}
+                                {this.state.userInfo && <div className="pull-right"><img style={{ margin: '1rem', width: '50px', height: '50px', borderRadius: '50%' }} src={this.state.userInfo.photoURL} /></div>}
+                            </Col>
+                        </Row>
+                    </header>
+                    {
+                        this.state.userInfo &&
+                        <div>
+                            {
+                                !this.state.showAddScore &&
+                                <Button bsStyle="primary" onClick={() => {this.toggleAddScore()}}>Add Score</Button>
+                            }
+    
+                            {
+                                this.state.showAddScore &&
+                                <EnterScore
+                                    userInfo={this.state.userInfo}
+                                    toggleAddScore={this.toggleAddScore.bind(this)}
+                                />
+                            }
+                        </div>
+                    }
+                    <LeaderBoard />
+                </div>
             </div>
         );
     }
