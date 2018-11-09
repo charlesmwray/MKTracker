eda4bc11bf2bf2a0d25613e85b682a8481b307bb
Charlie Wray <charlesmwray@gmail.com>
2018-11-09 12:26:18 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/MKTracker/precache-manifest.bc27f4fb7d86232430cd672a9b6b66bc.js"
+  "/MKTracker/precache-manifest.2be6b21266036ac70a8474ddceeb2b89.js"
 );
 
 workbox.clientsClaim();
